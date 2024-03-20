76e748464c51d89798c7bcb1d2811068e74aca58
majid598 <143610202+majid598@users.noreply.github.com>
2024-03-20 15:20:03 +0630

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ import { IoMdMail } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <div className="w-full relative lg:h-[60vh] border-t p-20">
+    <div className="w-full relative lg:flex md:flex lg:h-[60vh] border-t p-20">
       <h1 className="text-3xl font-semibold">Contact me</h1>
-      <div className="flex lg:flex-row md:flex-row flex-col lg:gap-32 lg:justify-between md:gap-24">
-        <div className="mt-12 lg:ml-40 md:ml-32">
+      <div className="lg:w-5/6 md:w-5/6 w-full flex lg:flex-row md:flex-row flex-col lg:gap-32 lg:justify-between md:gap-24">
+        <div className="mt-20">
           <a
             href="https://www.linkedin.com/in/razvanbotea/"
             className="flex items-center gap-1"
@@ -23,7 +23,7 @@ const Footer = () => {
             <IoMdMail /> contact@razvanbotea.me
           </a>
         </div>
-        <div className="lg:w-1/4 mt-10 lg:mt-0 mx-20">
+        <div className="lg:w-1/4 w-full mt-10 lg:mt-0 mx-20">
           <img
             src={"./assets/manoncall.png"}
             className="w-full h-full object-contain"

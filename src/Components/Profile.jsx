a8833ff71e6060e439ce509c810d91fb1881b7c5
majid598 <143610202+majid598@users.noreply.github.com>
2024-03-23 18:37:42 +0630

    updated

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const Profile = () => {
   return (
     <div className="w-full mx-auto flex-col items-center py-10 justify-center lg:justify-stretc relative flex">
-      <div className="profile w-52 h-52 bg-[#061731] cursor-pointer overflow-hidden border hover:scale-110 relative hover:-translate-y-5 transition-all duration-300 mt-10 rounded-full">
+      <div className="profile w-52 h-52 z-50 bg-[#061731] cursor-pointer overflow-hidden border hover:scale-110 relative hover:-translate-y-5 transition-all duration-300 mt-10 rounded-full">
         <img
           src="./assets/razvan.jpeg"
           className="w-full h-full object-contain"
@@ -12,10 +12,10 @@ const Profile = () => {
         />
       </div>
       <div className="w-full flex bg-whit flex-col text-center justify-center mt-5">
-        <h2 className="lg:text-3xl md:text-3xl sm:text-2xl text-xl font-semibold">
+        <h2 className="lg:text-3xl md:text-3xl sm:text-2xl text-xl font-semibold relative z-50">
           I'm Răzvan Botea
         </h2>
-        <h1 className="lg:text-6xl lg:h-16 md:h-16 sm:h-12 h-10 w-full daFont md:text-4xl sm:text-2xl text-xl flex items-center lg:gap-4 md:gap-3 gap-2 justify-center tracking-tighter font-bold uppercase mt-3">
+        <h1 className="lg:text-6xl lg:h-16 md:h-16 sm:h-12 h-10 w-full daFont md:text-4xl relative z-50 sm:text-2xl text-xl flex items-center lg:gap-4 md:gap-3 gap-2 justify-center tracking-tighter font-bold uppercase mt-3">
           AWS DevOps
           <div
             id="div"
